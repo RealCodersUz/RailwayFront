29913c44270321b4f6c0bea8dc26ed4edba0fc1e
Muhammadjon Abduvahobov <115058559+Megacoderuzb@users.noreply.github.com>
2023-11-28 00:52:47 +0500

    main page is in proccess

--- src/components/reportCards/ReportCards.jsx
+++ src/components/reportCards/ReportCards.jsx
@@ -1,30 +1,71 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import SideBar from "../sidebar";
 
 const reportsData = [
   { name: "Отчеты", url: "/reports" },
   { name: "Форма 69", url: "/forma69" },
-  { name: "data1", url: "http" },
-  { name: "data1", url: "http" },
-  { name: "data1", url: "http" },
+  { name: "Основные инструменты", url: "/tools" },
+  { name: "Материальный отчет", url: "/mreport" },
+  { name: "Налог", url: "/nalog" },
 ];
 
 function ReportCards() {
   return (
     <>
-      <div className="container p-5">
-        <div className="card p-5">
-          <h2 className="text-center">Отчеты</h2>
+      <div className="d-flex flex-row">
+        <SideBar />
+        <div className="container p-5">
+          <div className=" p-5">
+            <h2 className="text-center">Отчеты</h2>
 
-          <h4 className="text-center pb-5">Отправлять отчеты</h4>
-          <div className="d-flex justify-content-between">
-            {reportsData.map((data, index) => (
-              <div className="card p-3" style={{ width: "12rem" }} key={index}>
-                <a href={data.url} className="text-decoration-none">
-                  <h5 className="card-title text-center">{data.name}</h5>
-                </a>
-              </div>
-            ))}
+            {/* <h4 className="text-center pb-5">Отправлять отчеты</h4> */}
+            <div className="d-flex justify-content-between">
+              {reportsData.map((data, index) => (
+                <div
+                  className="card p-3"
+                  style={{ width: "12rem" }}
+                  key={index}
+                >
+                  <a href={data.url} className="text-decoration-none">
+                    <h5 className="card-title text-center">{data.name}</h5>
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className=" p-5">
+            <h2 className="text-center p-3">Архив</h2>
+            <div className="input-group my-5">
+              <select
+                className="form-control mx-3 rounded border-primary"
+                name="fileType"
+                id="filetype"
+              >
+                {reportsData.map((data, index) => (
+                  <option className="p-3" style={{ width: "2rem" }} key={index}>
+                    {data.name}
+                  </option>
+                ))}
+              </select>
+              <input
+                type="date"
+                className="form-control mx-3 rounded border-primary"
+              />
+              <button className="btn btn-primary rounded" type="button">
+                Button
+              </button>
+            </div>
+            {/* <form></form> */}
+          </div>
+          <div className="p-5 w-100 ">
+            <div className="w-100 border-bottom border-secondary d-flex flex-row justify-content-between ">
+              <p className=" fw-bold fs-3 text-black">topilgan file</p>
+              <button className="btn btn-primary h-25 align-center">
+                Pakazat
+              </button>
+              {/* <input className="form-control" type="file" id="formFile" /> */}
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,35 @@
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+  CDBSidebarFooter,
+} from "cdbreact";
+
+const SideBar = () => {
+  return (
+    <CDBSidebar height="1000">
+      <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
+        Contrast
+      </CDBSidebarHeader>
+      <CDBSidebarContent>
+        <CDBSidebarMenu>
+          <CDBSidebarMenuItem icon="th-large">Dashboard</CDBSidebarMenuItem>
+          <CDBSidebarMenuItem icon="sticky-note">Components</CDBSidebarMenuItem>
+          <CDBSidebarMenuItem icon="credit-card" iconType="solid">
+            Metrics
+          </CDBSidebarMenuItem>
+        </CDBSidebarMenu>
+      </CDBSidebarContent>
+
+      <CDBSidebarFooter style={{ textAlign: "center" }}>
+        <div className="sidebar-btn-wrapper" style={{ padding: "20px 5px" }}>
+          Sidebar Footer
+        </div>
+      </CDBSidebarFooter>
+    </CDBSidebar>
+  );
+};
+
+export default SideBar;
