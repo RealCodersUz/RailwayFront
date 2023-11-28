bf3083323f500ba705a7802ee5ff871961524d73
Muhammadjon Abduvahobov <115058559+Megacoderuzb@users.noreply.github.com>
2023-11-28 22:05:49 +0500

    sidebar'

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -9,25 +9,49 @@ import {
 
 const SideBar = () => {
   return (
-    <CDBSidebar height="1000">
+    <CDBSidebar
+      style={{
+        // height: "100%",
+        // position: "fixed",
+        // top: "7rem",
+        // left: "0",
+        display: "block",
+        maxWidth: "15rem",
+      }}
+      textColor="#333"
+      backgroundColor="#f0f0f0"
+      className="sticky-bottom"
+    >
       <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
-        Contrast
+        Contrast Light Mode
       </CDBSidebarHeader>
       <CDBSidebarContent>
         <CDBSidebarMenu>
           <CDBSidebarMenuItem icon="th-large">Dashboard</CDBSidebarMenuItem>
           <CDBSidebarMenuItem icon="sticky-note">Components</CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon="credit-card" iconType="solid">
+          <CDBSidebarMenuItem icon="chart-line" iconType="solid">
             Metrics
+          </CDBSidebarMenuItem>
+        </CDBSidebarMenu>
+        <CDBSidebarMenu style={{ visibility: "hidden" }}>
+          <CDBSidebarMenuItem icon="th-large">Dashboard</CDBSidebarMenuItem>
+          <CDBSidebarMenuItem icon="sticky-note">Components</CDBSidebarMenuItem>
+          <CDBSidebarMenuItem icon="chart-line" iconType="solid">
+            qaaaaaaaaaa
           </CDBSidebarMenuItem>
         </CDBSidebarMenu>
       </CDBSidebarContent>
 
       <CDBSidebarFooter style={{ textAlign: "center" }}>
         <div className="sidebar-btn-wrapper" style={{ padding: "20px 5px" }}>
-          Sidebar Footer
+          logout
         </div>
       </CDBSidebarFooter>
+      {/* <CDBSidebarFooter style={{ paddingLeft: "2rem" }}>
+        <div className="sidebar-btn-wrapper" style={{ padding: "20px 5px" }}>
+          Sidebar Footer
+        </div>
+      </CDBSidebarFooter> */}
     </CDBSidebar>
   );
 };
